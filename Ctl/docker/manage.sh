f1ae5673b237b55466b55cb22d0740db2ebcb058
Elliot Frank <elliot.g.frank@gmail.com>
2021-02-16 13:50:09 -0600

    edit template

--- Ctl/docker/manage.sh
+++ Ctl/docker/manage.sh
@@ -1,7 +1,5 @@
 SERVICE_HOME={{ service_home }}
-{% raw %}
-. $SERVICE_HOME/venv/bin/activate
-cd $SERVICE_HOME/main
+. "$SERVICE_HOME/{{ venv }}/bin/activate"
+cd "$SERVICE_HOME/main"
 
 ./manage.py $@
-{% endraw %}
